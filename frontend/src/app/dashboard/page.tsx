b636dd1beb3cc49c62c9409b335b223eea8b3981
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-12-20 22:46:34 +0700

    fix bugs

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,27 +1,37 @@
-"use client"
+"use client";
 import { SidebarProvider, SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
-import { AppSidebar } from "./_sidebar/app-sidebar";
-import ProductScreen from "./_app/page";
 import { useState } from "react";
+import ProductScreen from "./_app/page";
+import { AppSidebar } from "./_sidebar/app-sidebar";
 
-const [screen, setScreen] = useState<'product' | 'bill'>('product');
+type ScreenType = 'product' | 'bill';
+
 export default function DashboardPage() {
+    const [screen, setScreen] = useState<ScreenType>('product');
+
     return (
         <SidebarProvider>
             <AppSidebar data={{ screen, setScreen }} />
-            <MainContent />
+            <MainContent data={{ screen, setScreen }} />
         </SidebarProvider>
     );
 }
 
-const MainContent = () => {
+interface MainContentProps {
+    data: {
+        screen: ScreenType;
+        setScreen: React.Dispatch<React.SetStateAction<ScreenType>>;
+    }
+}
+
+const MainContent = (props: MainContentProps) => {
     const { open } = useSidebar();
 
     return (
         <main className={`${open ? 'w-[calc(100vw-16rem)]' : 'w-[100vw]'} h-screen transition-all duration-300`}>
             <SidebarTrigger className="absolute z-10" />
-            {screen === 'product' ? <ProductScreen /> : null}
-            {screen === 'bill' ? <div>Bill</div> : null}
+            {props.data.screen === 'product' ? <ProductScreen /> : null}
+            {props.data.screen === 'bill' ? <div>Bill</div> : null}
         </main>
     );
 };
